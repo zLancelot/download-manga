c56e2823133c2045e2830dd79418c1f17dcaf2f4
john <joao.prata@delfosim.com>
2020-03-11 18:00:48 -0300

    Function download chapters in range

--- src/main.py
+++ src/main.py
@@ -8,6 +8,28 @@ def print_info_mangas(mangas):
         print (f'ID: {manga["id"]}')
         print (f'Name: {manga["name"]}')
 
+def download_chapters_in_range(manga, initial, final):
+    for c in range(initial, final+1):
+        chapter = f'{"0"*(3 - len(str(c)))}{c}'
+        
+        file_name = f'{manga["name"]}_{chapter}.jpg'
+
+        url = f'http://{manga["host"]}.centraldemangas.com.br/{manga["name_url"]}/{manga["name_url"]}{chapter}-01.jpg'
+
+        headers = {
+            "Referer": f'http://centraldemangas.online/titulos/{manga["name_referer_header"]}/manga/ler-online/{chapter}'
+        }
+
+        print (url)
+        print (headers)
+        print ("\n")
+
+        response = requests.get(url, stream=True, headers=headers)
+
+        with open(file_name, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+
 with open('data/mangas.json', 'r') as f:
     mangas = json.load(f)
 
@@ -27,17 +49,9 @@ while (1):
         print ("_"*50)
         print ("\n\n  # Choose an ID below #")
 
+print ("\nDigite o Intervalo de capítulos que deseja baixar. Ex: 1-5 ou 20-50")
+range_chapters = input("Intervalo: ")
 
-print (manga)
+(chapter_initial, chapter_end) = map(int, range_chapters.split("-"))
 
-url = f'http://mangas2019.centraldemangas.com.br/{manga["name_url"]}/{manga["name_url"]}196-01.jpg'
-
-headers = {
-    "Referer": f'http://centraldemangas.online/titulos/{manga["name_referer_header"]}/manga/ler-online/196'
-}
-
-response = requests.get(url, stream=True, headers=headers)
-
-with open('img1.jpg', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response
+download_chapters_in_range(manga, chapter_initial, chapter_end)
